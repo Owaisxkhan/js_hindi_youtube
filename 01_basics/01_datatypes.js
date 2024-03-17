697904fa1a961e851bc090815bb8572a918f2d93
Owaisxkhan <163728517+Owaisxkhan@users.noreply.github.com>
2024-03-17 12:10:21 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as per newer version
+// //alert(4+4)// we are using nodejs ,  not browser
+
+console.log(3+3);
+//code readability should be high
+console.log("Owais")
+
+let name = "Owais"
+let age = 18
+let IsLoggedIn = false
+
+//number => 2 to power 53
+// bight
+// string => ""
+// boolean => fals/true
+// null => stand alone value (empty)
+// undefined => variable given but no value defined
+// symbol => unique
+
+
+//object 
+console.log(typeof "Owais")
+console.log(typeof null)
