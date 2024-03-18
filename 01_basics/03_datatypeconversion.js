274839020fad1ec3c8b49832a150142aeffc67a8
Owaisxkhan <163728517+Owaisxkhan@users.noreply.github.com>
2024-03-18 11:37:13 +0000

    operations

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -3,11 +3,11 @@
 // let score = undefined
 let score = true
 
-console.log(typeof score);
+// console.log(typeof score);
  
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -21,10 +21,52 @@ let isLoggedIn = 1
 // "Owais" => true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// ********************** Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1 = "Hello"
+let str2 = " Owais"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log( (3+4) * 5 % 3 )
+
+// console.log(+true);
+// console.log(true+);
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+// link to study in video
